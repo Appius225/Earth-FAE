b249ad53b34afa8ee41bbfbf4e95db15d53e8763
Matthew <mguid2088@gmail.com>
2019-03-13 20:15:53 -0500

    Add initial Meta classes

---
+++ Earth-FAE/Assets/Meta.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meta
+{
+    public enum TileType { CITY, PLAYER, ENEMY, ROCK, WATER, DEFAULT };
+    public bool blocked;
+    public bool onFire = false;
+    public TileType type;
+    
+    public Meta(TileType type = TileType.DEFAULT, bool blocked = false, bool onFire = false)
+    {
+        this.blocked = blocked;
+        this.onFire = onFire;
+        this.type = type;
+    }
+}
+ 
---
+++ Earth-FAE/Assets/MetaContainer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetaContainer : MonoBehaviour
+{
+    public Meta[,] metaData= new Meta[5,5]
+    {
+        { new Meta(), new Meta(), new Meta(), new Meta(), new Meta()},
+        { new Meta(), new Meta(), new Meta(), new Meta(), new Meta()},
+        { new Meta(), new Meta(), new Meta(), new Meta(), new Meta()},
+        { new Meta(), new Meta(), new Meta(), new Meta(), new Meta()},
+        { new Meta(), new Meta(), new Meta(), new Meta(), new Meta()},
+    };
+}
