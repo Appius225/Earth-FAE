99fc57372de64819c536cce22cd43aa01729a0de
jdav239 <38485509+jdav239@users.noreply.github.com>
2019-04-22 14:42:58 -0500

    Add sorta weapon buttons

--- Earth-FAE/Assets/Scripts/LevelSelectController.cs
+++ Earth-FAE/Assets/Scripts/LevelSelectController.cs
@@ -10,6 +10,12 @@ public class LevelSelectController : MonoBehaviour
     public GameObject[] tanks;
     private int[] tankLoad = new int[3];
     private GameObject[] iTank = new GameObject[3];
+    private int[] tank1 = new int[2];
+    private int weap1Num = 0;
+    private int[] tank2 = new int[2];
+    private int weap2Num = 0;
+    private int[] tank3 = new int[2];
+    private int weap3Num = 0;
 
     public void loadTank1(int var)
     {
@@ -23,21 +29,134 @@ public class LevelSelectController : MonoBehaviour
     {
         tankLoad[2] = var;
     }
+
+    public void tank1Weap(int weap)
+    {
+        tank1[weap1Num] = weap;
+        weap1Num++;
+        if (weap1Num == 2)
+        {
+            weap1Num = 0;
+        }
+    }
+    public void tank2Weap(int weap)
+    {
+        tank2[weap2Num] = weap;
+        weap2Num++;
+        if(weap2Num == 2)
+        {
+            weap2Num = 0;
+        }
+    }
+    public void tank3Weap(int weap)
+    {
+        tank3[weap3Num] = weap;
+        weap3Num++;
+        if(weap3Num == 2)
+        {
+            weap3Num = 0;
+        }
+    }
+
     public void PlayGame()
     {
-        if (iTank[0] == null && iTank[1] == null && iTank[2] == null)
+        for (int i = 0; i < 3; i++)
         {
-            iTank[0] = Instantiate(tanks[tankLoad[0]], new Vector3(100, 100, 0), Quaternion.identity);
-            iTank[1] = Instantiate(tanks[tankLoad[1]], new Vector3(100, 100, 0), Quaternion.identity);
-            iTank[2] = Instantiate(tanks[tankLoad[2]], new Vector3(100, 100, 0), Quaternion.identity);
-            DontDestroyOnLoad(iTank[0]);
-            DontDestroyOnLoad(iTank[1]);
-            DontDestroyOnLoad(iTank[2]);
+            if(iTank[0] == null && iTank[1] == null && iTank[2] == null)
+            {
 
-            metadata.Tanks[0] = iTank[0];
-            metadata.Tanks[1] = iTank[1];
-            metadata.Tanks[2] = iTank[2];
+                iTank[i] = Instantiate(tanks[tankLoad[i]], new Vector3(100, 100, 0), Quaternion.identity);
+            }
+            DontDestroyOnLoad(iTank[i]);
+            Tank t = iTank[i].GetComponent(typeof(Tank)) as Tank;
+            if (i == 0) //tank 1
+            {
+                if (tank1[0] == 0) //weapon 1 initialization
+                {
+                    t.weap1 = new normalShot();
+                }
+                else if (tank1[0] == 1)
+                {
+
+                }
+                else
+                {
+
+                }
+
+                if (tank1[1] == 0) //weapon 2 initialization
+                {
+                    t.weap2 = new normalShot();
+                }
+                else if (tank1[1] == 1)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+            else if (i == 2) //tank 2
+            {
+                if (tank2[0] == 0) //weapon 1 initialization
+                {
+                    t.weap1 = new normalShot();
+                }
+                else if (tank2[0] == 1)
+                {
+
+                }
+                else
+                {
+
+                }
+
+                if (tank2[1] == 0) //weapon 2 initialization
+                {
+                    t.weap2 = new normalShot();
+                }
+                else if (tank2[1] == 1)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+            else //tank 3
+            {
+                if (tank3[0] == 0) //weapon 1 initialization
+                {
+                    t.weap1 = new normalShot();
+                }
+                else if (tank3[0] == 1)
+                {
+
+                }
+                else
+                {
+
+                }
+
+                if (tank3[1] == 0) //weapon 2 initialization
+                {
+                    t.weap2 = new normalShot();
+                }
+                else if (tank3[1] == 1)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
         }
+        metadata.Tanks[0] = iTank[0];
+        metadata.Tanks[1] = iTank[1];
+        metadata.Tanks[2] = iTank[2];
         SceneManager.LoadScene(level);
     }
     public void LoadScene(int level)
